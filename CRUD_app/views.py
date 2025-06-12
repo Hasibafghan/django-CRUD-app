368def2591253c19fe93a3e531c7669c164904d6
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-06-12 06:26:30 +0430

    Sign in & Sign up UI Created

--- CRUD_app/views.py
+++ CRUD_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import login , logout , authenticate
 
 # Create your views here.
 def home(request):
     return render(request , 'home.html' , {})
+
+
+def login_user(request):
+    return render(request , 'login_user.html' , {})
+
+def logout_user(request):
+    logout(request)
+    return render(request , 'home.html' , {})
+
+def signup_user(request):
+    return render(request , 'signup_user.html' , {})
+
